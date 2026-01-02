f4a661d283ec3107cc088ec61021ac8ebe3afc8b
Avi Shukla <avishuklacode@gmail.com>
2026-01-02 22:52:49 +0530

    Update script.js

--- Mappy/script.js
+++ Mappy/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//selecting the element
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
