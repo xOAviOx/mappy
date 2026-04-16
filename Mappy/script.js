0267939a4bb1e4c2fbe98214f01530ea21aea77a
Avi Shukla <avishuklacode@gmail.com>
2026-04-16 23:15:59 +0530

    Update script.js

--- Mappy/script.js
+++ Mappy/script.js
@@ -21,7 +21,7 @@ class Workout {
   }
 
   _setDescription() {
-    // prettier-ignore
+    // prettier-ignor//
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
